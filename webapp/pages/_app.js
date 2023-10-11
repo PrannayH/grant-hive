8922837f57b1e9afb3f8da06e56e7d8638535c5c
prannayh <prannayh88@gmail.com>
2023-10-11 21:23:41 +0530

    homepage frontend

--- webapp/pages/_app.js
+++ webapp/pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+// pages/_app.js
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Marck+Script&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
