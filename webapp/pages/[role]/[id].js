4f29e61e1c20f272e063823b1d9aa436b58b4e75
prannayh <prannayh88@gmail.com>
2023-10-17 21:37:58 +0530

    updated frontend

--- webapp/pages/[role]/[id].js
+++ webapp/pages/[role]/[id].js
@@ -6,7 +6,7 @@ function roles(){
     const router = useRouter();
     const { role, id } = router.query;
 
-    const [queryResult, setQueryResult] = useState();
+    const [queryResult, setQueryResult] = useState([]);
     const [actionState, setActionState] = useState("+");
     const [grantType, setGrantType] = useState("program");
     const [fundOpen, setFundOpen] = useState(false)
@@ -87,65 +87,203 @@ function roles(){
     }
 
     return(<div>
-        {role=='applicant'?<div>
+        {role === 'applicant' ? (
             <div>
-                <div>Hello {localStorage.getItem("loggedInUser")}</div>
-                <button onClick={toggleActionState}>{actionState}</button>
-            </div>
-            <div>
-                {actionState=="x"?<div>
-                    <div>Create Grant Proposal</div>
-                    <div>Proposal Name</div>
-                    <div><input></input></div>
-                    <div>Required Budget</div>
-                    <div><input></input></div>
-                    <div>Proposal Description</div>
-                    <div><input></input></div>
-                </div>:
+                <nav className="navbar-log">
+                    <div className="logo2">
+                        <h1>GrantHive</h1>
+                    </div>
+                </nav>
                 <div>
-                    <div><button onClick={toggleGrantType}>Your existing {grantType}</button></div>
-                    {grantType=="proposal"?<div>
-                        <div>some sort a map function to render all PROPOSAL data. design the div by yourself</div>
-                    </div>:
-                    <div>
-                        <div>some sort a map function to render all PROGRAM + REVIEW(rating) + FUNDER(fund amount) data. design the div by yourself</div>
-                    </div>}
-                </div>}
+                    <div className="user-greeting">Hello <span>{localStorage.getItem("loggedInUser")}</span>!</div>
+                    <button className="toggle-button" onClick={toggleActionState}>
+                        {actionState}
+                    </button>
+                </div>
+                <div>
+                    {actionState === "x" ? (
+                        <div className="proposal-form">
+                            <div style={{ fontSize: '20px', textAlign: 'center', margin: '1% auto', fontWeight: 'bold', color:'#703131' }}>Create Grant Proposal</div>
+                            <div style={{ marginTop: '3%' }}>
+                                <div className="input-pair">
+                                    <div>Proposal Name :</div>
+                                    <div>
+                                        <input type="text" />
+                                    </div>
+                                </div>
+                                <div className="input-pair">
+                                    <div>Required Budget :</div>
+                                    <div>
+                                        <input type="text" />
+                                    </div>
+                                </div>
+                                <div className="input-pair">
+                                    <div>Proposal Description :</div>
+                                    <div>
+                                        <textarea rows="4" cols="30" style={{ border: '1px solid black' }}></textarea>
+                                    </div>
+                                </div>
+                                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                    <button style={{
+                                        padding: '10px 20px',
+                                        background: '#582626',
+                                        color: '#fff',
+                                        border: 'none',
+                                        cursor: 'pointer',
+                                        borderRadius: '5px',
+                                        fontSize: '12px',
+                                        fontWeight: 'bold'
+                                    }}>Submit</button>
+                                </div>
+                            </div>
+
+                        </div>
+                    ) : (
+                        <div className="existing-data">
+                            <div>
+                                <button className="toggle-data-button" onClick={toggleGrantType}>
+                                    Your existing {grantType}(s)
+                                </button>
+                            </div>
+                            {grantType === "proposal" ? (
+                                    <div>
+                                        {queryResult.map((item, index) => (
+                                            <div key={index} className="proposal-item">                                                
+                                                {/* <div className="hmm">Program ID: {item.program_id}</div> */}
+                                                <div className="hmm">Proposal ID: {item.proposal_id}</div>
+                                                <div className="hmm">Proposal Title: {item.proposal_title}</div>
+                                                <div className="hmm">Project Description: {item.project_description}</div>
+                                                <div className="hmm">Required Budget: {item.required_budget}</div>
+                                            </div>
+                                        ))}
+                                    </div>
+                            ) : (
+                                    <div>
+                                        {queryResult.map((item, index) => (
+                                            <div key={index} className="program-item1">
+                                                <div className="hmm">ID: {item.id}</div>
+                                                <div className="hmm">Abstract: {item.abstract}</div>
+                                                <div className="hmm">Grant Amount: {item.grant_amount}</div> 
+                                                <div className="hmm">Organisation Name: {item.organisation_name}</div>
+                                                <div className="hmm">Program Name: {item.program_name}</div>
+                                                <div className="hmm">Approval date: {item.approval_date}</div>
+                                                <div className="hmm">Feedback: {item.feedback}</div>
+                                                <div className="hmm">Deadline: {item.deadline}</div>
+                                                <div className="hmm">Funded By: {item.funded_by_whom}</div>
+                                                <div className="hmm">Fund Duration: {item.fund_duration}</div>
+                                                <div className="hmm">Review Date: {item.review_date}</div>
+                                                <div className="hmm">Reviewed By: {item.review_whom}</div>
+                                                <div className="hmm">Review Score: {item.review_score}</div>
+
+                                            </div>
+                                        ))}
+                                    </div>
+                            )}
+                        </div>
+                    )}
+                </div>
             </div>
-        </div>:<div></div>}
+        ) : (
+            <div></div>
+        )}
+
         {role=='reviewer'?<div>
+            <nav className='navbar-log'>
+                <div className="logo2">
+                    <h1>
+                        GrantHive
+                    </h1>
+                </div>
+            </nav>
             <div>
-                <div>Hello {localStorage.getItem("loggedInUser")}</div>
+                <div className="user-greeting">Hello {localStorage.getItem("loggedInUser")}!</div>
                 {/* <button onClick={toggleActionState}>{actionState}</button> */}
-            </div>
-            <div>
-                some sort a map function to render all GRANT PROPOSAL data(so reviewers can approve/reject). design the div by yourself
-                <div>
-                    <div>Enter Rating</div>
-                    <div><input></input></div>
-                    <button>Approve</button>
-                    <button>Reject</button>
+            </div> 
+            <div className="rev-container">
+                {/* Display the program items with buttons on the right */}
+                <div className="rev-info">
+                    {queryResult.map((item, index) => (
+                        <div key={index} className="rev-item">
+                            <div className="aa">
+                                <div className="hm">Proposal ID: {item.id}</div>
+                                <div className="hm">Proposal Title: {item.proposal_title}</div>
+                                <div className="hm">Organisation Name: {item.organisation_name}</div>
+                                <div className="hm">Team Leader: {item.team_lead_fname} {item.team_lead_lname}</div>
+                                {/* <div className="hm">Proposal ID: {item.id}</div> */}
+                                <div className="hm">Project Description: {item.project_description}</div>
+                                <div className="hm">Required Budget: {item.required_budget}</div>
+                            </div>
+                            <div className="rev-content">
+                                <div className="rev-details">
+                                    <div className="rev-detail">
+                                        <label className="rev-lab" htmlFor="rating">Enter Rating</label>
+                                        <input type="number" id="rating" min="1" max="5" step="0.1" />
+                                    </div>
+                                    <div className="rev-actions">
+                                        <button className="approve-button">Approve</button>
+                                        <button className="reject-button">Reject</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
+
+            
+
         </div>:<div></div>}
-        {role=='funder'?<div>
+        {role === 'funder' ? (
             <div>
-                <div>Hello {localStorage.getItem("loggedInUser")}</div>
-                {/* <button onClick={toggleActionState}>{actionState}</button> */}
-            </div>
-            <div>
-                some sort a map function to render all GRANT PROGRAM data(so funders can approve/reject + decide amount). design the div by yourself
+                <nav className='navbar-log'>
+                    <div className="logo2">
+                        <h1>GrantHive</h1>
+                    </div>
+                </nav>
                 <div>
-                    <button onClick={()=>{setFundOpen(!fundOpen)}}>Want to Fund?</button>
-                    {fundOpen?<div>
-                        <div>Set Amount</div>
-                        <div><input></input></div>
-                        <button>Approve</button>
-                    </div>:
-                    <button>Reject</button>}
+                    <div className="user-greeting">Hello {localStorage.getItem("loggedInUser")}!</div>
+                </div>
+                <div className="grant-program-container">
+                    {/* Display the program items with buttons on the right */}
+                    <div className="grant-program-info">
+                        {queryResult.map((item, index) => (
+                            <div key={index} className="program-item">
+                                <div className = "aa">
+                                <div className="hm">Program ID: {item.id}</div>
+                                <div className="hm">Organisation Name: {item.organisation_name}</div>
+                                <div className="hm">Team Leader: {item.team_lead_fname} {item.team_lead_lname}</div>
+                                <div className="hm">Proposal Title: {item.proposal_title}</div>                                                             
+                                <div className="hm">Program Name: {item.program_name}</div>
+                                {/* <div className="hm">Abstract: {item.abstract}</div> */}
+                                <div className="hm">Project Description: {item.project_description}</div>
+                                <div className="hm">Required Budget: {item.required_budget}</div>
+                                <div className="hm">Approval Date: {item.approval_date}</div>
+                                <div className="hm">Deadline: {item.deadline}</div>
+                                <div className="hm">Progress: {item.progress}</div>
+                                </div>
+                                <div className="grant-program-actions">
+                                    <button className="fund-button" onClick={() => { setFundOpen(!fundOpen) }}>Want to Fund?</button>
+                                    {fundOpen ? (
+                                        <div className="fund-form">
+                                            <div className="set-amount-label">Set Amount</div>
+                                            <div className="set-amount-input">
+                                                <input type="text" />
+                                            </div>
+                                            <button className="approve1-button">Approve</button>
+                                        </div>
+                                    ) : (
+                                        <button className="reject1-button">Reject</button>
+                                    )}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>:<div></div>}
+        ) : (
+            <div></div>
+        )}
+
 
     </div>)
 }
